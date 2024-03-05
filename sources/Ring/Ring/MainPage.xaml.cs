741917192221d8cb85d0f0672b53d96d0486a01b
mattpat48 <matteo.patella@studio.unibo.it>
2024-03-05 17:56:54 +0100

    #5732 button now send a msg to local docker

--- sources/Ring/Ring/MainPage.xaml.cs
+++ sources/Ring/Ring/MainPage.xaml.cs
@@ -4,10 +4,22 @@ namespace Ring
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage(MainViewModel viewModel)
         {
             InitializeComponent();
+            // faccio binding del viewmodel con la pagina
             BindingContext = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var viewModel = BindingContext as MainViewModel;
+            if (viewModel != null)
+            {
+                await viewModel.Connect();
+            }
         }
     }
 }
--- sources/Ring/Ring/ViewModel/MainViewModel.cs
+++ sources/Ring/Ring/ViewModel/MainViewModel.cs
@@ -1,27 +1,96 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Text.Json;
 
 namespace Ring.ViewModel;
 
 public partial class MainViewModel : ObservableObject
 {
 
-    public MainViewModel()
+    // proprietà osservabili del bottone
+    [ObservableProperty]
+    string buttonText;
+    [ObservableProperty]
+    bool buttonIsEnabled;
+
+    // proprietà osservabile del testo di errore
+    [ObservableProperty]
+    string connectionErrorText;
+    [ObservableProperty]
+    string messageErrorText;
+
+    // proprietà osservabile del messaggio
+    [ObservableProperty]
+    string messageText;
+
+
+    private MqttFactory mqttServer;
+    private IMqttClient mqttClient;
+    private MqttClientOptions options;
+
+    public async Task Connect()
     {
-        Text = "Open";
-        IsEnabled = true;
+        ConnectionErrorText = "";
+        try
+        {
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+            await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("ringRequest/request").Build());
+        }
+        catch (Exception ex)
+        {
+            ConnectionErrorText = ex.Message;
+        }
     }
 
-    [ObservableProperty]
-    string text;
+    // costruttore per il viewmodel
+    public MainViewModel()
+    {
+        ButtonText = "Open";
+        ButtonIsEnabled = true;
 
-    [ObservableProperty]
-    bool isEnabled;
+        ConnectionErrorText = "";
+        MessageErrorText = "";
+        MessageText = "";
+        
+        // inizializzo il client e server mqtt
+        mqttServer = new MqttFactory();
+        mqttClient = mqttServer.CreateMqttClient();
+
+        // inizializzo le opzioni del server mqtt
+        options = new MqttClientOptionsBuilder()
+            // ATTENZIONE: inserire l'indirizzo IP del server MQTT
+            .WithTcpServer("10.20.100.50", 1883)
+            //.WithCredentials("username", "password")
+            .Build();
+    }
 
     [RelayCommand]
-    void Open()
+    async Task Open()
     {
-        Text = "Opening";
-        IsEnabled = false;
+        MessageErrorText = "";
+
+        var toSend = new
+        {
+            id = "IdDevice",
+            datetime = "DateTime",
+            lat = "Lat",
+            language = "Lng",
+            key = "Key",
+        };
+
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("ringRequest/request")
+            .WithPayload(toSend.ToString())
+            .Build();
+        try
+        {
+            await mqttClient.PublishAsync(message);
+        }
+        catch (Exception ex)
+        {
+            MessageErrorText = ex.Message;
+        }
     }
 }
