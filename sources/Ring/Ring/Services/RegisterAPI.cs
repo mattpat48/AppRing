4e3f2a3f8331e69d25cfeea22dc9af98f33fd013
mattpat48 <matteo.patella@studio.unibo.it>
2024-03-12 15:25:19 +0100

    using improve

--- sources/Ring/Ring/Services/RegisterAPI.cs
+++ sources/Ring/Ring/Services/RegisterAPI.cs
@@ -93,65 +93,56 @@ public class RegisterAPI
                 // cifro il contenuto con la chiave pubblica del server
                 if (!string.IsNullOrEmpty(rsaPublicKey))
                 {
-                    using (RSA rsa = RSA.Create(4096))
+                    using RSA rsa = RSA.Create(4096);
+                    rsa.ImportFromPem(rsaPublicKey.ToCharArray());
+
+                    // genero una chiave simmetrica per AES
+                    using Aes aes = Aes.Create();
+                    aes.GenerateKey();
+                    aes.GenerateIV();
+
+                    // cripto i dati usando AES
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                    using MemoryStream msEncrypt = new();
+                    using (CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
-                        rsa.ImportFromPem(rsaPublicKey.ToCharArray());
+                        using StreamWriter swEncrypt = new(csEncrypt);
+                        // Scrivi tutti i dati da criptare nel flusso.
+                        swEncrypt.Write(json);
+                    }
+                    encryptedData = msEncrypt.ToArray();
+                    encryptedAesKey = rsa.Encrypt(aes.Key, RSAEncryptionPadding.Pkcs1);
+                    encryptedAesIV = rsa.Encrypt(aes.IV, RSAEncryptionPadding.Pkcs1);
 
-                        // genero una chiave simmetrica per AES
-                        using (Aes aes = Aes.Create())
-                        {
-                            aes.GenerateKey();
-                            aes.GenerateIV();
+                    // converto i dati cifrati in base64
+                    string encryptedDataBase64 = Convert.ToBase64String(encryptedData);
+                    string encryptedAesKeyBase64 = Convert.ToBase64String(encryptedAesKey);
+                    string encryptedAesIVBase64 = Convert.ToBase64String(encryptedAesIV);
 
-                            // cripto i dati usando AES
-                            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-                            using (MemoryStream msEncrypt = new MemoryStream())
-                            {
-                                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                                {
-                                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                                    {
-                                        // Scrivi tutti i dati da criptare nel flusso.
-                                        swEncrypt.Write(json);
-                                    }
-                                }
-                                encryptedData = msEncrypt.ToArray();
-                                encryptedAesKey = rsa.Encrypt(aes.Key, RSAEncryptionPadding.Pkcs1);
-                                encryptedAesIV = rsa.Encrypt(aes.IV, RSAEncryptionPadding.Pkcs1);
+                    // creo il contenuto da inviare al server
+                    var requestBody = new
+                    {
+                        EncryptedData = encryptedDataBase64,
+                        EncryptedKey = encryptedAesKeyBase64,
+                        EncryptedIV = encryptedAesIVBase64
+                    };
+                    string jsonRequestBody = JsonConvert.SerializeObject(requestBody);
+                    var stringContent = new StringContent(jsonRequestBody, Encoding.UTF8, "application/json");
 
-                                // converto i dati cifrati in base64
-                                string encryptedDataBase64 = Convert.ToBase64String(encryptedData);
-                                string encryptedAesKeyBase64 = Convert.ToBase64String(encryptedAesKey);
-                                string encryptedAesIVBase64 = Convert.ToBase64String(encryptedAesIV);
+                    // invio il contenuto cifrato al server
+                    HttpResponseMessage response = await _httpClient.PostAsync(url, stringContent);
+                    var responseContent = await response.Content.ReadAsStringAsync();
 
-                                // creo il contenuto da inviare al server
-                                var requestBody = new
-                                {
-                                    EncryptedData = encryptedDataBase64,
-                                    EncryptedKey = encryptedAesKeyBase64,
-                                    EncryptedIV = encryptedAesIVBase64
-                                };
-                                string jsonRequestBody = JsonConvert.SerializeObject(requestBody);
-                                var stringContent = new StringContent(jsonRequestBody, Encoding.UTF8, "application/json");
-
-                                // invio il contenuto cifrato al server
-                                HttpResponseMessage response = await _httpClient.PostAsync(url, stringContent);
-                                var responseContent = await response.Content.ReadAsStringAsync();
-
-                                if (response.IsSuccessStatusCode)
-                                {
-                                    // se ho successo apro la pagina di verifica
-                                    return "success";
-                                }
-                                else
-                                {
-                                    SecureStorage.Remove("ServerKey");
-                                    SecureStorage.Remove("PhoneNumber");
-                                    return responseContent;
-                                }
-                            }
-                        }
-
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // se ho successo apro la pagina di verifica
+                        return "success";
+                    }
+                    else
+                    {
+                        SecureStorage.Remove("ServerKey");
+                        SecureStorage.Remove("PhoneNumber");
+                        return responseContent;
                     }
                 }
                 else
